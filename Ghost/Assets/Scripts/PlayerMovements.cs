5c7ceee1ccccbb84f8683fc3b8ce8d25fec1c6f1
asherfsantos <40329281+asherfsantos@users.noreply.github.com>
2018-06-16 21:03:17 -0700

    Initial commit

---
+++ Ghost/Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour 
+{
+	private Rigidbody2D player;
+	public float speed;
+	public bool playerInPortal;
+	public Sprite spiritSprite;
+	public SpriteRenderer playerRenderer;
+	public int collissions = 0;
+
+	void Start () 
+	{
+		player = GetComponent<Rigidbody2D>();
+		playerRenderer = player.GetComponent<SpriteRenderer>();
+	}
+	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		player.AddForce(movement * speed);
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	void onTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.CompareTag("Portal"))
+		{
+			collissions ++;
+		}
+	}
+}
