73892961837f67dd10ea7753629ff25fc0b55612
pappahthecat <39333315+pappahthecat@users.noreply.github.com>
2018-06-17 13:50:52 -0700

    Credit Scene Return

---
+++ Ghost/Assets/Scripts/CreditScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScene : MonoBehaviour {
+
+    public void changemenuscreen(string scenename)
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
